dabad04adedf97beb569344c0b3a1f826418b960
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2020-01-31 17:18:16 +0000

    fast changes speed distributions stuff

---
+++ Bonsai-0802/Extensions/roundToNearestVal.cs
@@ -0,0 +1,18 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class roundToNearestVal
+{
+    public double roundVal {get; set; }
+    public IObservable<double> Process(IObservable<double> source)
+    {
+        return source.Select(value => Math.Round(value/roundVal)*roundVal);
+    }
+}
